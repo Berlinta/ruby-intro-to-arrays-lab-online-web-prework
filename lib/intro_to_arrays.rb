85dfec6b47daccd664bb29c4e6358435273ca9e0
Nash Brancic <nashfft@gmail.com>
2019-01-29 05:26:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -25,19 +25,18 @@ def south_east_asia
   array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 end
 
-def first_element_with_array_methods=first_country
-  ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+def first_element_with_array_methods(first_country)
   first_country.first
 end
 
-def last_country(array)
-  ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  array.last
+def last_element_with_array_methods(last_country)
+  last_country.last
 end
 
-def length(array)
-  ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  array.length
+def programming_languages
+  array = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
 end
 
-  
+def length_of_array(programming_languages)
+  programming_languages.size
+end
