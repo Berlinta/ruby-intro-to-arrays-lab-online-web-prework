49fbd2fc0bb092e608e41c31c9cbc7de7c418191
Nash Brancic <nashfft@gmail.com>
2019-01-29 05:25:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  ["Cars","Trucks"]
+end
+
+def first_element(array)
+  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[0]
+end
+
+def third_element(array)
+  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[2]
+end
+
+def last_element(array)
+  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[-1]
+end
+
+
+def first_element_with_array_methods=first_country
+  ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country.first
+end
+
+def last_country(array)
+  ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.last
+end
+
+def length(array)
+  ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  array.length
+end
+
+  
